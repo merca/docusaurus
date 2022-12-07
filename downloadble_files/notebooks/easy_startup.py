44b55000738098a144cdc91f017f6330b1733da8
Merca Ovnerud <merca666@gmail.com>
2022-12-07 03:59:21 +0000

    Add day 3 of code

--- downloadble_files/notebooks/easy_startup.py
+++ downloadble_files/notebooks/easy_startup.py
@@ -9,21 +9,32 @@ from pyspark.sql.types import *
 # COMMAND ----------
 # DBTITLE 1,Read dataframe from blob storage
 
+
 def read_df_from_blob_storage(account_name, container_name, file_name, file_type):
-    df = (
-        spark.read.format(file_type)
-        .load("abfss://" + container_name + "@" + account_name + ".blob.core.windows.net/" + file_name)
+    df = spark.read.format(file_type).load(
+        "abfss://"
+        + container_name
+        + "@"
+        + account_name
+        + ".blob.core.windows.net/"
+        + file_name
     )
     return df
 
+
 # COMMAND ----------
 # DBTITLE 1,Use this function to get data from blob storage
-df = read_df_from_blob_storage("account_name", "container_name", "file_name", "file_type")
+df = read_df_from_blob_storage(
+    "account_name", "container_name", "file_name", "file_type"
+)
 
 # COMMAND ----------
 # DBTITLE 1,Write dataframe to blob storage as delta table and register in metastore
 
-df.write.saveAsTable(name="table_name",path="abfss://container_name@account_name.blob.core.windows.net/folder")
+df.write.saveAsTable(
+    name="table_name",
+    path="abfss://<container_name>@<account_name>.blob.core.windows.net/folder",
+)
 
 # COMMAND ----------
 # DBTITLE 1,Read dataframe from metastore
